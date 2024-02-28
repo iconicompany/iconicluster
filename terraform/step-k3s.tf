9f052eecc0b8b4d63141ca55f61b43a3f2a319f5
Viacheslav Borisov <slavb18@iconicompany.com>
2024-02-28 11:01:21 +0400

    reformat step-k3s

--- terraform/step-k3s.tf
+++ terraform/step-k3s.tf
@@ -1,20 +1,20 @@
 locals {
-  certificates_names           = var.SERVERS_NUM>0 ? ["client-ca", "server-ca", "request-header-ca", "etcd/peer-ca", "etcd/server-ca"] : []
-  certificates_types           = { for s in local.certificates_names : index(local.certificates_names, s) => s }
-  source_ca_path               = "${var.STEPCERTPATH}/k3s"
-  target_ca_path               = "/var/lib/rancher/k3s/server/tls"
+  certificates_names = var.SERVERS_NUM > 0 ? ["client-ca", "server-ca", "request-header-ca", "etcd/peer-ca", "etcd/server-ca"] : []
+  certificates_types = { for s in local.certificates_names : index(local.certificates_names, s) => s }
+  source_ca_path     = "${var.STEPCERTPATH}/k3s"
+  target_ca_path     = "/var/lib/rancher/k3s/server/tls"
 }
 
 # tokens for CA
 data "external" "step_k3s_ca_token" {
   for_each = local.certificates_types
-  program = ["bash", "${path.module}/step-ca-token.sh"]
+  program  = ["bash", "${path.module}/step-ca-token.sh"]
 
   query = {
-    STEPPATH     = var.STEPPATH
-    STEP_PROVISIONER = var.STEP_PROVISIONER_KUBE
+    STEPPATH           = var.STEPPATH
+    STEP_PROVISIONER   = var.STEP_PROVISIONER_KUBE
     STEP_PASSWORD_FILE = var.STEP_PASSWORD_KUBE
-    CN            = replace(each.value, "/", "-")
+    CN                 = replace(each.value, "/", "-")
   }
 }
 
@@ -24,11 +24,11 @@ resource "null_resource" "step_k3s_ca" {
 
   for_each = local.certificates_types
   connection {
-        #host     = resource.terraform_data.hostname[0].output
-        # line below not working when SERVERS_NUM=0
-        host      = rustack_vm.cluster[0].floating_ip
-        #host      = var.CLUSTER_DOMAIN
-        user      = var.USER_LOGIN
+    #host     = resource.terraform_data.hostname[0].output
+    # line below not working when SERVERS_NUM=0
+    host = rustack_vm.cluster[0].floating_ip
+    #host      = var.CLUSTER_DOMAIN
+    user = var.USER_LOGIN
   }
 
   provisioner "remote-exec" {
@@ -36,7 +36,7 @@ resource "null_resource" "step_k3s_ca" {
       "sudo mkdir -p ${local.source_ca_path}/$(dirname ${each.value}) ${local.target_ca_path}/$(dirname ${each.value})",
       "sudo env STEP_TOKEN=${data.external.step_k3s_ca_token[each.key].result.TOKEN} step ca certificate ${replace(each.value, "/", "-")} ${local.source_ca_path}/${each.value}.crt ${local.source_ca_path}/${each.value}.key --provisioner ${var.STEP_PROVISIONER_KUBE}",
       "sudo cp ${local.source_ca_path}/${each.value}.crt ${local.target_ca_path}/${each.value}.crt",
-      "sudo cp ${local.source_ca_path}/${each.value}.key ${local.target_ca_path}/${each.value}.key" 
+      "sudo cp ${local.source_ca_path}/${each.value}.key ${local.target_ca_path}/${each.value}.key"
     ]
   }
 
