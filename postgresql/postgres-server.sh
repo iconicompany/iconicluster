bf2776e066fba1be5092b31a8c3211886619789d
Viacheslav Borisov <slavb18@iconicompany.com>
2024-02-29 12:50:15 +0400

    Enhance PostgreSQL server configuration for SSL and network accessibility

--- postgresql/postgres-server.sh
+++ postgresql/postgres-server.sh
@@ -1,2 +1,27 @@
-sudo apt update
-sudo apt install postgresql postgresql-client
+set -e
+
+function error_exit() {
+  echo "$1" 1>&2
+  exit 1
+}
+
+function check_deps() {
+  test -f $(which step) || error_exit "step command not detected in path, please install it"
+}
+
+sudo apt install  --no-upgrade postgresql postgresql-client
+
+PG_HBA=/etc/postgresql/14/main/pg_hba.conf
+PG_HBA_CONFIG="hostssl all             all             all                     cert clientcert=verify-full clientname=DN  map=iconicompany"
+
+PG_IDENT=/etc/postgresql/14/main/pg_ident.conf
+PG_IDENT_CONFIG='iconicompany    "/^CN=(.*),OU=users,O=iconicompany,C=ru\$"    \1'
+
+# enable SSL
+if ! sudo grep -Fq iconicompany $PG_HBA; then
+    echo  $PG_HBA_CONFIG | sudo tee -a $PG_HBA
+fi
+if ! sudo grep -Fq iconicompany $PG_IDENT; then
+    echo $PG_IDENT_CONFIG | sudo tee -a $PG_IDENT
+fi
+sudo sed -i "s/#listen_addresses = 'localhost'/listen_addresses = '*'/" /etc/postgresql/14/main/postgresql.conf
