2fe27e3b43a5ec372a704c169d342407f069b028
Viacheslav Borisov <slavb18@iconicompany.com>
2024-10-11 14:53:12 +0400

    update

--- step-ca/install/step-cli-user.sh
+++ step-ca/install/step-cli-user.sh
@@ -20,10 +20,16 @@ while getopts ${optstring} arg; do
 done
 
 if ! command -v step > /dev/null; then
-    deb=$(mktemp --suffix .deb)
-    wget -O $deb https://dl.smallstep.com/cli/docs-ca-install/latest/step-cli_amd64.deb
-    sudo dpkg -i $deb
-    rm -f $deb
+    if ! command -v dpkg > /dev/null; then
+        LATEST_STEP_VERSION=$(curl -s https://api.github.com/repos/smallstep/cli/releases/latest | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
+        wget -qO - https://dl.smallstep.com/gh-release/cli/gh-release-header/${LATEST_STEP_VERSION}/step_linux_${LATEST_STEP_VERSION:1}_amd64.tar.gz | tar zxvf -  -C /tmp/
+        sudo mv /tmp/step_${LATEST_STEP_VERSION:1}/bin/step /usr/local/bin
+    else
+        deb=$(mktemp --suffix .deb)
+        wget -O $deb https://dl.smallstep.com/cli/docs-ca-install/latest/step-cli_amd64.deb
+        sudo dpkg -i $deb
+        rm -f $deb
+    fi
 fi
 set -e
 
