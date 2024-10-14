547fa336ef092097d9831ff693345e3a05317513
Viacheslav Borisov <slavb18@iconicompany.com>
2024-10-14 16:11:48 +0400

    update

--- backup/pgbackup.sh
+++ backup/pgbackup.sh
@@ -1,13 +1,12 @@
 # add -a for data only
 umask 027
 mkdir -p /var/backups/local/postgres
-chgrp backup /var/backups/local/postgres
-while  [ ! $# -eq 0 ] ; do
-    DBNAME=$1
+#chgrp backup /var/backups/local/postgres
+DB_LIST=$(psql  -tAc "SELECT datname FROM pg_database WHERE datistemplate = false;")
+for DBNAME in $DB_LIST; do
     BACKUPFILE=/var/backups/local/postgres/${DBNAME}_`date +"%FT%H%M%S"`.sql
     pg_dump $DBNAME > $BACKUPFILE
     gzip $BACKUPFILE
-    chgrp backup $BACKUPFILE.gz
-    shift
+    #chgrp backup $BACKUPFILE.gz
 done
 find /var/backups/local/postgres -type f -mtime +10 -delete
