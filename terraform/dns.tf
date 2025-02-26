27da7104a5c1a4628cd6421f7fa24da3f2c55c30
Viacheslav Borisov <slavb18@iconicompany.com>
2025-02-26 11:15:44 +0400

    add agent dns

--- terraform/dns.tf
+++ terraform/dns.tf
@@ -10,6 +10,13 @@ resource "rustack_dns_record" "node_ws_record" {
   host   = "${resource.terraform_data.hostname[count.index].output}."
   data   = resource.rustack_vm.cluster[count.index].floating_ip
 }
+resource "rustack_dns_record" "agent_ws_record" {
+  count  = var.AGENTS_NUM
+  dns_id = data.rustack_dns.cluster_dns.id
+  type   = "A"
+  host   = "${resource.terraform_data.agentname[count.index].output}."
+  data   = resource.rustack_vm.agent[count.index].floating_ip
+}
 
 resource "rustack_dns_record" "cluster_ws_record" {
   count  = var.SERVERS_NUM
